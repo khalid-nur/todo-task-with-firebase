83d8dc66fb4946f88d64a00f25b54cd843f59d7c
khalid-nur <khalidnur150@gmail.com>
2023-03-29 09:39:32 -0500

    Add task description and update task status functionalities to the task list items to enhance the user experience and allow users to add descriptions to tasks

--- js/script.js
+++ js/script.js
@@ -31,6 +31,9 @@ const queryRef = query(colRef, orderBy("createdAt", "desc"));
 const textInputEl = document.querySelector(".input-form");
 const taskListEl = document.querySelector(".task-list");
 const taskCountEl = document.querySelector(".task-count");
+const taskDescEl = document.querySelector(".task-desc");
+const colorSwitch = document.querySelector("#input-color-switch");
+
 // Add Task
 const addTask = function () {
   textInputEl.addEventListener("submit", function (e) {
@@ -63,6 +66,7 @@ const getTasks = function () {
       tasks.push({ id: doc.id, ...doc.data() });
     });
     generateTasks(tasks);
+    generateTaskDescriptions(tasks);
   });
 };
 
@@ -73,7 +77,9 @@ const generateTasks = function (taskItems) {
 
   taskItems.forEach((taskItem) => {
     taskItemsHTML += `
-    <li class="task-item">
+    <li class="task-item ${taskItem.open === true ? "active" : ""} "data-id="${
+      taskItem.id
+    }">
       <div class="task-item-container">
         <div class="checkbox-container">
           <div class="check-mark ${
@@ -86,12 +92,42 @@ const generateTasks = function (taskItems) {
         <button class="close-btn "data-id="${taskItem.id}"">x</button>
       </div>
   </li>
-    
     `;
   });
   taskListEl.innerHTML = taskItemsHTML;
   createEventListeners();
   taskCount(taskItems);
+};
+
+// Display task description
+const generateTaskDescriptions = function (taskItems) {
+  let taskDescHTML = "";
+  taskItems.forEach((taskItem) => {
+    taskDescHTML += `
+    <div class="task-desc-container ${
+      taskItem.open === true ? "open" : ""
+    } "data-id="${taskItem.id}" ">
+      <p class="timestamp">
+        <span class="created"> Created:</span>
+        <span class="current-date">${moment(
+          taskItem.createdAt?.toDate()
+        ).calendar()}</span>
+      </p>
+      <div class="task-desc-textarea">
+        <form class="textarea-form">
+          <textarea
+            name="details"
+            class="task-desc-textarea-input "data-id="${taskItem.id}" "
+            placeholder="Task Description"
+          >${taskItem.description || ""}</textarea>
+        </form>
+      </div>
+  </div>
+    `;
+
+    taskDescEl.innerHTML = taskDescHTML;
+  });
+  createEventListeners();
 };
 
 // Event Handlers
@@ -101,6 +137,52 @@ const createEventListeners = function () {
   checkMarks.forEach((checkMark) => {
     checkMark.addEventListener("click", function () {
       checkMarkComplete(checkMark.dataset.id);
+    });
+  });
+
+  // Toggle the delete button
+  const taskListItemEls = document.querySelectorAll(".task-item");
+  // console.log(taskListItemEls);
+  taskListItemEls.forEach((taskListItemEl) => {
+    const taskCloseBtns = taskListItemEl.querySelector(".close-btn");
+    taskListItemEl.addEventListener("mouseover", function (e) {
+      taskCloseBtns.style.display = "inline-block";
+    });
+
+    taskListItemEl.addEventListener("mouseleave", function () {
+      taskCloseBtns.style.display = "none";
+    });
+  });
+
+  // Display a specific task description
+  taskListItemEls.forEach((currentTaskListItemEl) => {
+    currentTaskListItemEl.addEventListener("click", function () {
+      console.log("clicked");
+      taskListItemEls.forEach((otherTaskListItemEl) => {
+        if (
+          otherTaskListItemEl.dataset.id !== currentTaskListItemEl.dataset.id
+        ) {
+          updateDoc(doc(db, "tasks", otherTaskListItemEl.dataset.id), {
+            open: false,
+          });
+        }
+      });
+      openTaskDescription(currentTaskListItemEl.dataset.id);
+    });
+  });
+
+  // Adding text to the description filed
+  const textareaEls = document.querySelectorAll(".task-desc-textarea-input");
+
+  textareaEls.forEach((textareaEl) => {
+    let textVale = "";
+    textareaEl.addEventListener("keydown", function (e) {
+      if (e.key === "Enter") {
+        textVale = textareaEl.value;
+        updateDoc(doc(db, "tasks", textareaEl.dataset.id), {
+          description: textVale,
+        });
+      }
     });
   });
 
@@ -133,6 +215,27 @@ const checkMarkComplete = function (checkMarkId) {
   });
 };
 
+// Open specific task description
+const openTaskDescription = function (TaskListItemsID) {
+  const docRefs = doc(db, "tasks", TaskListItemsID);
+  console.log(TaskListItemsID);
+  getDoc(docRefs)
+    .then((doc) => {
+      if (doc.data().open === true) {
+        updateDoc(docRefs, {
+          open: false,
+        });
+      } else if (doc.data().open === false) {
+        updateDoc(docRefs, {
+          open: true,
+        });
+      }
+    })
+    .catch((err) => {
+      console.log("error");
+    });
+};
+
 // Delete a specific task
 const deleteTask = function (deleteBtnId) {
   const docRef = doc(db, "tasks", deleteBtnId);
@@ -141,7 +244,6 @@ const deleteTask = function (deleteBtnId) {
 
 // Display count of tasks
 const taskCount = function (taskItems) {
-  const taskCountEl = document.querySelector(".task-count");
   taskCountEl.innerHTML = taskItems.length;
 };
 
